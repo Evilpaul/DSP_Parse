98fe182f5aaf3967aa3a78aad5182c7d7a560083
Evilpaul <yj589794@googlemail.com>
2014-09-19 18:48:26 +0100

    Read in an RTF file and output the text lines

--- DSP_Parse/Program.cs
+++ DSP_Parse/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace DSP_Parse
 {
@@ -10,6 +8,24 @@ namespace DSP_Parse
 	{
 		static void Main(string[] args)
 		{
+			// If your RTF file isn't in the same folder as the .exe file for the project,  
+			// specify the path to the file in the following assignment statement.  
+			string path = @"test.rtf";
+
+			//Create the RichTextBox. (Requires a reference to System.Windows.Forms.)
+			System.Windows.Forms.RichTextBox rtBox = new System.Windows.Forms.RichTextBox();
+
+			// Get the contents of the RTF file. When the contents of the file are   
+			// stored in the string (rtfText), the contents are encoded as UTF-16.  
+			string rtfText = System.IO.File.ReadAllText(path);
+
+			// Use the RichTextBox to convert the RTF code to plain text.
+			rtBox.Rtf = rtfText;
+
+			foreach (string line in rtBox.Lines)
+			{
+				Console.WriteLine(line);
+			}
 		}
 	}
 }
